cb8e991858f144d10620db6172c99114698f28d9
Hammad Ali <hammad.a22002@gmail.com>
2022-08-14 11:39:31 +0500

    Update Thumb.tsx

--- app/components/Thumb.tsx
+++ app/components/Thumb.tsx
@@ -6,15 +6,8 @@ type PropTypes = {
   image: string | undefined | null;
 };
 export default function Thumb({ image }: PropTypes) {
-  return (
-    <Image
-      style={styles.image}
-      source={{
-        uri: image ? image : require("../assets/no_image.jpg"),
-      }}
-      resizeMode="cover"
-    />
-  );
+  const source = image ? { uri: image } : require("../assets/no_image.jpg");
+  return <Image style={styles.image} source={source} resizeMode="cover" />;
 }
 
 const styles = StyleSheet.create({
