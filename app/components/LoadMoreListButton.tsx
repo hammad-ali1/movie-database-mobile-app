96fcc1c7f020c25dbd26770f67f219eea357573b
Hammad Ali <hammad.a22002@gmail.com>
2022-08-14 22:08:52 +0500

    Update LoadMoreListButton.tsx

--- app/components/LoadMoreListButton.tsx
+++ app/components/LoadMoreListButton.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import colors from "../config/colors";
 //PropTypes
 type PropTypes = { onPress?: onPressHandler };
@@ -7,6 +8,7 @@ export default function ({ onPress }: PropTypes) {
   return (
     <View style={[styles.container]}>
       <TouchableOpacity style={styles.touchableContainer} onPress={onPress}>
+        <AntDesign style={styles.icon} name="arrowright" size={50} />
         <Text style={styles.text}>LOAD MORE</Text>
       </TouchableOpacity>
     </View>
@@ -20,9 +22,17 @@ const styles = StyleSheet.create({
   touchableContainer: {
     flex: 1,
     justifyContent: "center",
+    alignContent: "center",
+    width: 100,
+  },
+  icon: {
+    color: colors.loadMoreList,
+    alignSelf: "center",
   },
   text: {
     color: colors.loadMoreList,
+    fontSize: 10,
+    alignSelf: "center",
     fontWeight: "bold",
   },
 });
