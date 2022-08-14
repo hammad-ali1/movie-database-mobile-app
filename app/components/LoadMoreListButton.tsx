9fa31a0c2de7b7c6998bdbe6536e9157b2b49418
Hammad Ali <hammad.a22002@gmail.com>
2022-08-14 20:02:59 +0500

    load more button

--- app/components/LoadMoreListButton.tsx
+++ app/components/LoadMoreListButton.tsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
-
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
+import colors from "../config/colors";
 //PropTypes
-type PropTypes = {};
+type PropTypes = { onPress?: onPressHandler };
 
-export default function ({}: PropTypes) {
-  return <View style={[styles.container]}></View>;
+export default function ({ onPress }: PropTypes) {
+  return (
+    <View style={[styles.container]}>
+      <TouchableOpacity style={styles.touchableContainer} onPress={onPress}>
+        <Text style={styles.text}>LOAD MORE</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  touchableContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  text: {
+    color: colors.loadMoreList,
+    fontWeight: "bold",
   },
 });
--- app/config/colors.ts
+++ app/config/colors.ts
@@ -4,10 +4,12 @@ const colors = {
   //horizontal scroll bar
   horizontalScrollBarBackGround: "rgba(52, 52, 52, 0.3)",
   horizontalScollTitle: "white",
+  loadMoreList: "rgba(49, 3, 110, 0.612)",
   //vertical scroll bar
   verticalScrollContainer: "rgba(0,0,0,0.2)", //container includes both text and list
   verticalScrollBackGround: "transparent",
   verticalScrollTitle: "black",
+
   //custom linear gradient
   linearGradient: ["#6e5b9b", "#7669a6"],
 
