7b9c63240f5ab97abf22c77009e6a2fcb26c7ed8
Hammad Ali <hammad.a22002@gmail.com>
2022-08-15 12:42:50 +0500

    thumb size option

--- app/components/Thumb.tsx
+++ app/components/Thumb.tsx
@@ -4,15 +4,34 @@ import sizes from "../config/sizes";
 //PropTypes
 type PropTypes = {
   image: string | undefined | null;
+  size?: "medium" | "big";
 };
-export default function Thumb({ image }: PropTypes) {
+export default function Thumb({ image, size }: PropTypes) {
+  let multiplyFactor;
+  let userStyles: any = {};
+  if (size === "medium") {
+    multiplyFactor = 1.5;
+  } else if (size === "big") {
+    multiplyFactor = 2;
+  }
+  if (multiplyFactor) {
+    userStyles["width"] = sizes.thumbWidth * multiplyFactor;
+    userStyles["height"] = sizes.thumbHeight * multiplyFactor;
+  }
+
   const source = image ? { uri: image } : require("../assets/no_image.jpg");
-  return <Image style={styles.image} source={source} resizeMode="cover" />;
+  return (
+    <Image
+      style={[styles.image, userStyles]}
+      source={source}
+      resizeMode="cover"
+    />
+  );
 }
 
 const styles = StyleSheet.create({
   image: {
-    flex: 1,
+    // flex: 1,
     width: sizes.thumbWidth * 1.2,
     height: sizes.thumbHeight * 1.2,
     marginRight: 10,
--- app/screens/MovieScreen.tsx
+++ app/screens/MovieScreen.tsx
@@ -1,6 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
+import { POSTER_SIZE, IMAGE_BASE_URL } from "../config/config";
+
 //Hooks
 import useMovieDetailsFetch from "../hooks/useMovieDetailsFetch";
+//components
+import Thumb from "../components/Thumb";
 //PropTypes
 type PropTypes = {};
 
@@ -9,7 +13,18 @@ export default function MovieScreen({ navigation, route }: RouteProps) {
   const { state: movie } = useMovieDetailsFetch(id);
   return (
     <View style={[styles.container]}>
-      <Text>{movie.original_title}</Text>
+      <View style={styles.movieDetails}>
+        <Thumb
+          size="medium"
+          image={
+            movie.poster_path
+              ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+              : undefined
+          }
+        />
+        <Text style={styles.title}>{movie.title}</Text>
+      </View>
+      <View style={styles.actors}></View>
     </View>
   );
 }
@@ -17,5 +32,17 @@ export default function MovieScreen({ navigation, route }: RouteProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  movieDetails: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  title: {
+    alignSelf: "center",
+    fontWeight: "bold",
+  },
+  actors: {
+    flex: 1,
+    backgroundColor: "blue",
   },
 });
